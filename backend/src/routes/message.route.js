299cfcef95eea5ea8f97ab4015454653ef330cd3
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-03-10 01:39:20 +0530

    feat: chatify backend:routes

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+// ROUTER
+const router = express.Router();
+
+// CONTROLLERS
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+
+export default router;
