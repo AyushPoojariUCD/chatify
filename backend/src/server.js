4998b45b73c231d483ec0f0baee0b97b8e6e7338
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-03-10 01:10:28 +0530

    feat: chatify backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,10 @@
-import cors from "cors";
-import dotenv from "dotenv";
 import express from "express";
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-dotenv.config();
-
-app.use(cors());
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
