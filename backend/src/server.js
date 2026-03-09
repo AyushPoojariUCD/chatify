bb9accbf6e3302722ba7999582a32460822ff090
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-03-10 01:35:42 +0530

    feat: chatify backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,27 @@
 import express from "express";
 
+// EXPRESS APP
 const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+// ROUTES
+app.use("api/auth", (req, res) => {
+  res.send("Auth route");
+});
+
+app.use("api/message", (req, res) => {
+  res.send("Message route");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
